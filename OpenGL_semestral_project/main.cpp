1f1bdd1d6f957e884771ad456811ae60b6e2b067
Denys S <counter297@gmail.com>
2024-05-09 21:33:01 +0200

    Add camera movement

--- OpenGL_semestral_project/main.cpp
+++ OpenGL_semestral_project/main.cpp
@@ -12,13 +12,17 @@
 #include "Mesh.h"
 #include "Shader.h"
 #include "Window.h"
-
+#include "Camera.h"
 
 const float toRadians = 3.14159265f / 180.0f;
 
 Window mainWindow;
 std::vector<Mesh*> meshList;
 std::vector<Shader> shaderList;
+Camera camera;
+
+GLfloat deltaTime = 0.0f;
+GLfloat lastTime = 0.0f;
 
 bool direction = true;
 float triOffset = 0.0f;
@@ -83,7 +87,10 @@ int main()
 	CreateObjects();
 	CreateShaders();
 
+	camera = Camera(glm::vec3(0.0f, 0.0f, -5.0f), glm::vec3(0.0f, 1.0f, 0.0f), -90.0f, 0.0f, 1.0f, 0.1f);
+
 	GLuint uniformProjection = 0;
+	GLuint uniformView = 0;
 	GLuint uniformModel = 0;
 
 	glm::mat4 projection = glm::perspective(45.0f, mainWindow.getBufferWidth() / mainWindow.getBufferHeight(), 0.1f, 1000.0f);
@@ -93,6 +100,13 @@ int main()
 	{
 		// Get + Handle user input events
 		glfwPollEvents();
+
+		GLfloat now = glfwGetTime();
+		deltaTime = now - lastTime;
+		lastTime = now;
+
+		camera.keyControl(mainWindow.getKeys(), deltaTime);
+		camera.mouseControl(mainWindow.getXChange(), mainWindow.getYChange());
 
 		// Update triangle offset for animation
 		if (direction)
@@ -135,6 +149,7 @@ int main()
 		shaderList[0].UseShader();
 		uniformModel = shaderList[0].GetModelLocation();
 		uniformProjection = shaderList[0].GetProjectionLocation();
+		uniformView = shaderList[0].GetViewLocation();
 
 		// Diagonal + rotational translations
 		glm::mat4 model = glm::mat4(1.0f);
@@ -147,6 +162,7 @@ int main()
 		// Set uniform variable value for xMove in vertex shader
 		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
 		glUniformMatrix4fv(uniformProjection, 1, GL_FALSE, glm::value_ptr(projection));
+		glUniformMatrix4fv(uniformView, 1, GL_FALSE, glm::value_ptr(camera.calculateViewMatrix()));
 		meshList[0]->RenderMesh();
 
 
@@ -155,6 +171,7 @@ int main()
 		model = glm::rotate(model, -curAngle * toRadians, glm::vec3(0.0f, 1.0f, 0.0f));
 		model = glm::scale(model, glm::vec3(0.4f, 0.4f, 1.0f));
 		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
+		//glUniformMatrix4fv(uniformProjection, 1, GL_FALSE, glm::value_ptr(camera.calculateViewMatrix()));
 		meshList[1]->RenderMesh();
 
 		// Unuse shader program
