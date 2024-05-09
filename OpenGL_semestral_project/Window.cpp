b2f0ce8f381168ae1f8fb7590e1e8f7eb15feddd
Denys S <counter297@gmail.com>
2024-05-09 20:03:46 +0200

    Move shader implementation to separate files. Cleanup main.cpp Add user input hook routines. Create simultaneous instance of two rotating triangle mesh.

---
+++ OpenGL_semestral_project/Window.cpp
@@ -0,0 +1,158 @@
+#include "Window.h"
+
+Window::Window()
+{
+	width = 800;
+	height = 600;
+
+	for (size_t i = 0; i < 1024; i++)
+	{
+		keys[i] = false;
+	}
+}
+
+Window::Window(GLint windowWidth, GLint windowHeight)
+{
+	width = windowWidth;
+	height = windowHeight;
+
+	for (size_t i = 0; i < 1024; i++)
+	{
+		keys[i] = false;
+	}
+}
+
+int Window::Initialize()
+{
+	// Initialise GLFW
+	if (!glfwInit())
+	{
+		printf("GLFW initialisation failed!");
+		glfwTerminate();
+		return 1;
+	}
+
+	// Setup GLFW window properties
+	// OpenGL version
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	// Core Profile
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	// Allow Forward Compatbility
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+	// Create the window
+	mainWindow = glfwCreateWindow(width, height, "Test Window", NULL, NULL);
+	if (!mainWindow)
+	{
+		printf("GLFW window creation failed!");
+		glfwTerminate();
+		return 1;
+	}
+
+	// Get Buffer Size information
+	glfwGetFramebufferSize(mainWindow, &bufferWidth, &bufferHeight);
+
+	// Set context for GLEW to use
+	glfwMakeContextCurrent(mainWindow);
+
+	//Handle key and mouse input
+	createCallbacks();
+
+	//Hide mouse cursor
+	glfwSetInputMode(mainWindow, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
+	// Allow modern extension features
+	glewExperimental = GL_TRUE;
+
+	/* OpenGL itself provides a standard set of functionality,
+	but it also allows for extensions,
+	which are additional features that may not be supported by all OpenGL implementations or versions.
+	GLEW helps manage these extensions by providing
+	a consistent way to access their functionality across different platforms and OpenGL versions. */
+	if (glewInit() != GLEW_OK)
+	{
+		printf("GLEW initialisation failed!");
+		glfwDestroyWindow(mainWindow);
+		glfwTerminate();
+		return 1;
+	}
+
+	// Enable Z-test
+	glEnable(GL_DEPTH_TEST);
+
+	// Setup Viewport size
+	glViewport(0, 0, bufferWidth, bufferHeight);
+
+
+	glfwSetWindowUserPointer(mainWindow, this);
+}
+
+void Window::createCallbacks()
+{
+	glfwSetKeyCallback(mainWindow, handleKeys);
+	glfwSetCursorPosCallback(mainWindow, handleMouse);
+}
+
+GLfloat Window::getXChange()
+{
+	GLfloat theChange = xChange;
+	xChange = 0.0f;
+	return theChange;
+}
+
+GLfloat Window::getYChange()
+{
+	GLfloat theChange = yChange;
+	yChange = 0.0f;
+	return theChange;
+}
+
+void Window::handleKeys(GLFWwindow* window, int key, int code, int action, int mode)
+{
+	Window* theWindow = static_cast<Window*>(glfwGetWindowUserPointer(window));
+
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+	{
+		glfwSetWindowShouldClose(window, GL_TRUE);
+	}
+
+	if (key >= 0 && key < 1024)
+	{
+		if (action == GLFW_PRESS)
+		{
+			theWindow->keys[key] = true;
+			//printf("Pressed: %d\n", key);
+		}
+		else if (action == GLFW_RELEASE)
+		{
+			theWindow->keys[key] = false;
+			//printf("Released: %d\n", key);
+		}
+	}
+}
+
+void Window::handleMouse(GLFWwindow* window, double xPos, double yPos)
+{
+	Window* theWindow = static_cast<Window*>(glfwGetWindowUserPointer(window));
+
+	if (theWindow->mouseFirstMoved)
+	{
+		theWindow->lastX = xPos;
+		theWindow->lastY = yPos;
+		theWindow->mouseFirstMoved = false;
+	}
+
+	theWindow->xChange = xPos - theWindow->lastX;
+	theWindow->yChange = theWindow->lastY - yPos;
+
+	theWindow->lastX = xPos;
+	theWindow->lastY = yPos;
+
+	//printf("x:%.6f, y:%.6f\n", theWindow->xChange, theWindow->yChange);
+}
+
+Window::~Window()
+{
+
+}
--- OpenGL_semestral_project/main.cpp
+++ OpenGL_semestral_project/main.cpp
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <cmath>
+#include <vector>
 
 #include <GL\glew.h>
 #include <GLFW\glfw3.h>
@@ -8,12 +9,16 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
+#include "Mesh.h"
+#include "Shader.h"
+#include "Window.h"
 
-// Window dimensions
-const GLint WIDTH = 800, HEIGHT = 600;
+
 const float toRadians = 3.14159265f / 180.0f;
 
-GLuint VBO, VAO, IBO, shader, uniformModel, uniformProjection;
+Window mainWindow;
+std::vector<Mesh*> meshList;
+std::vector<Shader> shaderList;
 
 bool direction = true;
 float triOffset = 0.0f;
@@ -28,35 +33,13 @@ float maxSize = 0.8f;
 float minSize = 0.1f;
 
 // Vertex Shader code
-static const char* vShader = "													\n\
-#version 330																	\n\
-																				\n\
-layout (location = 0) in vec3 pos;												\n\
-																				\n\
-out vec4 vCol;																	\n\
-uniform mat4 model;																\n\
-uniform mat4 projection;														\n\
-																				\n\
-void main()																		\n\
-{																				\n\
-    gl_Position = projection * model * vec4(pos, 1.0);							\n\
-	vCol = vec4(clamp(pos, 0.0f, 1.0f), 1.0);									\n\
-}";
+static const char* vShader = "shader/shader.vert";
 
 // Fragment Shader
-static const char* fShader = "													\n\
-#version 330																	\n\
-																				\n\
-in  vec4 vCol;																	\n\
-out vec4 colour;																\n\
-																				\n\
-void main()																		\n\
-{																				\n\
-    colour = vCol;																\n\
-}";
+static const char* fShader = "shader/shader.frag";
 
 // Function to create a triangle
-void CreateTriangle()
+void CreateObjects()
 {
 	unsigned int indices[] = {
 		0, 3, 1,
@@ -73,173 +56,40 @@ void CreateTriangle()
 		0.0f, 1.0f, 0.0f
 	};
 
-	// Generate vertex array object (VAO)
-	glGenVertexArrays(1, &VAO);
-	glBindVertexArray(VAO);
+	Mesh* obj1 = new Mesh();
+	obj1->CreateMesh(vertices, indices, 12, 12);
+	meshList.push_back(obj1);
 
-	// Generate element buffer object (IBO)
-	glGenBuffers(1, &IBO);
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IBO);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-
-	// Generate vertex buffer object (VBO)
-	glGenBuffers(1, &VBO);
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-	// Specify vertex attribute pointers
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
-	glEnableVertexAttribArray(0);
-
-	// Unbind VBO, IBO and VAO
-	glBindBuffer(GL_ARRAY_BUFFER, 0);
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
-	glBindVertexArray(0);
+	Mesh* obj2 = new Mesh();
+	obj2->CreateMesh(vertices, indices, 12, 12);
+	meshList.push_back(obj2);
 }
 
-// Function to add a shader to a program
-void AddShader(GLuint theProgram, const char* shaderCode, GLenum shaderType)
+void CreateShaders()
 {
-	// Create shader object
-	GLuint theShader = glCreateShader(shaderType);
-
-	// Set shader source code
-	const GLchar* theCode[1];
-	theCode[0] = shaderCode;
-
-	GLint codeLength[1];
-	codeLength[0] = strlen(shaderCode);
-
-	glShaderSource(theShader, 1, theCode, codeLength);
-	glCompileShader(theShader);
-
-	GLint result = 0;
-	GLchar eLog[1024] = { 0 };
-
-	// Check shader compilation status
-	glGetShaderiv(theShader, GL_COMPILE_STATUS, &result);
-	if (!result)
-	{
-		glGetShaderInfoLog(theShader, 1024, NULL, eLog);
-		fprintf(stderr, "Error compiling the %d shader: '%s'\n", shaderType, eLog);
-		return;
-	}
-
-	// Attach the shader to the program
-	glAttachShader(theProgram, theShader);
+	Shader* shader1 = new Shader();
+	shader1->CreateFromFiles(vShader, fShader);
+	shaderList.push_back(*shader1);
 }
-
-// Function to compile shaders and link them into a program
-void CompileShaders()
-{
-	// Create shader program
-	shader = glCreateProgram();
-
-	if (!shader)
-	{
-		printf("Failed to create shader\n");
-		return;
-	}
-
-	// Add vertex and fragment shaders to the program
-	AddShader(shader, vShader, GL_VERTEX_SHADER);
-	AddShader(shader, fShader, GL_FRAGMENT_SHADER);
-
-	GLint result = 0;
-	GLchar eLog[1024] = { 0 };
-
-	// Link the shader program
-	glLinkProgram(shader);
-	glGetProgramiv(shader, GL_LINK_STATUS, &result);
-	if (!result)
-	{
-		glGetProgramInfoLog(shader, sizeof(eLog), NULL, eLog);
-		printf("Error linking program: '%s'\n", eLog);
-		return;
-	}
-
-	// Validate the shader program
-	glValidateProgram(shader);
-	glGetProgramiv(shader, GL_VALIDATE_STATUS, &result);
-	if (!result)
-	{
-		glGetProgramInfoLog(shader, sizeof(eLog), NULL, eLog);
-		printf("Error validating program: '%s'\n", eLog);
-		return;
-	}
-
-	// ...
-	uniformModel = glGetUniformLocation(shader, "model");
-	uniformProjection = glGetUniformLocation(shader, "projection");
-}
-
 
 // Main function
 int main()
 {
-	// Initialise GLFW
-	if (!glfwInit())
-	{
-		printf("GLFW initialisation failed!");
-		glfwTerminate();
-		return 1;
-	}
 
-	// Setup GLFW window properties
-	// OpenGL version
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	// Core Profile
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-	// Allow Forward Compatbility
-	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-
-	// Create the window
-	GLFWwindow* mainWindow = glfwCreateWindow(WIDTH, HEIGHT, "Test Window", NULL, NULL);
-	if (!mainWindow)
-	{
-		printf("GLFW window creation failed!");
-		glfwTerminate();
-		return 1;
-	}
-
-	// Get Buffer Size information
-	int bufferWidth, bufferHeight;
-	glfwGetFramebufferSize(mainWindow, &bufferWidth, &bufferHeight);
-
-	// Set context for GLEW to use
-	glfwMakeContextCurrent(mainWindow);
-
-	// Allow modern extension features
-	glewExperimental = GL_TRUE;
-
-	/* OpenGL itself provides a standard set of functionality,
-	but it also allows for extensions,
-	which are additional features that may not be supported by all OpenGL implementations or versions.
-	GLEW helps manage these extensions by providing
-	a consistent way to access their functionality across different platforms and OpenGL versions. */
-	if (glewInit() != GLEW_OK)
-	{
-		printf("GLEW initialisation failed!");
-		glfwDestroyWindow(mainWindow);
-		glfwTerminate();
-		return 1;
-	}
-
-	// Enable Z-test
-	glEnable(GL_DEPTH_TEST);
-
-	// Setup Viewport size
-	glViewport(0, 0, bufferWidth, bufferHeight);
+	mainWindow = Window(800, 600);
+	mainWindow.Initialize();
 
 	// Create triangle and compile shaders
-	CreateTriangle();
-	CompileShaders();
+	CreateObjects();
+	CreateShaders();
 
-	glm::mat4 projection = glm::perspective(45.0f, (GLfloat)bufferWidth / (GLfloat)bufferHeight, 0.1f, 1000.0f);
+	GLuint uniformProjection = 0;
+	GLuint uniformModel = 0;
+
+	glm::mat4 projection = glm::perspective(45.0f, mainWindow.getBufferWidth() / mainWindow.getBufferHeight(), 0.1f, 1000.0f);
 
 	// Loop until window closed
-	while (!glfwWindowShouldClose(mainWindow))
+	while (!mainWindow.getShouldClose())
 	{
 		// Get + Handle user input events
 		glfwPollEvents();
@@ -282,7 +132,9 @@ int main()
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 		// Use shader program
-		glUseProgram(shader);
+		shaderList[0].UseShader();
+		uniformModel = shaderList[0].GetModelLocation();
+		uniformProjection = shaderList[0].GetProjectionLocation();
 
 		// Diagonal + rotational translations
 		glm::mat4 model = glm::mat4(1.0f);
@@ -292,27 +144,23 @@ int main()
 		//model = glm::scale(model, glm::vec3(curSize, curSize, 1.0f));
 		//model = glm::translate(model, glm::vec3(triOffset, triOffset, 0.0f));
 
-
-
 		// Set uniform variable value for xMove in vertex shader
 		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
 		glUniformMatrix4fv(uniformProjection, 1, GL_FALSE, glm::value_ptr(projection));
+		meshList[0]->RenderMesh();
 
-		// Bind VAO and draw triangle
-		glBindVertexArray(VAO);
-		//glDrawArrays(GL_TRIANGLES, 0, 3);
-		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IBO);
 
-		glDrawElements(GL_TRIANGLES, 12, GL_UNSIGNED_INT, 0);
-
-		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
-		glBindVertexArray(0);
+		model = glm::mat4(1.0f);
+		model = glm::translate(model, glm::vec3(-triOffset, triOffset, -2.5f));
+		model = glm::rotate(model, -curAngle * toRadians, glm::vec3(0.0f, 1.0f, 0.0f));
+		model = glm::scale(model, glm::vec3(0.4f, 0.4f, 1.0f));
+		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
+		meshList[1]->RenderMesh();
 
 		// Unuse shader program
 		glUseProgram(0);
 
-		// Swap front and back buffers
-		glfwSwapBuffers(mainWindow);
+		mainWindow.swapBuffers();
 	}
 
 	// Terminate GLFW
