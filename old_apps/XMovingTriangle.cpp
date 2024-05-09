f4b0392dd8610eacb119f3ac4752b09ff1625bee
Denys S <counter297@gmail.com>
2024-05-09 19:52:31 +0200

    Add perspective transformation

--- old_apps/XMovingTriangle.cpp
+++ old_apps/XMovingTriangle.cpp
@@ -4,28 +4,16 @@
 
 #include <GL\glew.h>
 #include <GLFW\glfw3.h>
-#include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
-#include <glm/gtc/type_ptr.hpp>
-
 
 // Window dimensions
 const GLint WIDTH = 800, HEIGHT = 600;
-const float toRadians = 3.14159265f / 180.0f;
 
-GLuint VBO, VAO, IBO, shader, uniformModel, uniformProjection;
+GLuint VBO, VAO, shader, uniformXMove;
 
 bool direction = true;
 float triOffset = 0.0f;
 float triMaxOffset = 0.7f;
 float triIncrement = 0.0005f;
-
-float curAngle = 0.0f;
-
-bool sizeDirection = true;
-float curSize = 0.4f;
-float maxSize = 0.8f;
-float minSize = 0.1f;
 
 // Vertex Shader code
 static const char* vShader = "													\n\
@@ -33,42 +21,30 @@ static const char* vShader = "													\n\
 																				\n\
 layout (location = 0) in vec3 pos;												\n\
 																				\n\
-out vec4 vCol;																	\n\
-uniform mat4 model;																\n\
-uniform mat4 projection;														\n\
+uniform float xMove;															\n\
 																				\n\
 void main()																		\n\
 {																				\n\
-    gl_Position = projection * model * vec4(pos, 1.0);							\n\
-	vCol = vec4(clamp(pos, 0.0f, 1.0f), 1.0);									\n\
+    gl_Position = vec4(0.4 * pos.x + xMove, 0.4 * pos.y, pos.z, 1.0);			\n\
 }";
 
 // Fragment Shader
 static const char* fShader = "													\n\
 #version 330																	\n\
 																				\n\
-in  vec4 vCol;																	\n\
 out vec4 colour;																\n\
 																				\n\
 void main()																		\n\
 {																				\n\
-    colour = vCol;																\n\
+    colour = vec4(1.0, 0.0, 0.0, 1.0);											\n\
 }";
 
 // Function to create a triangle
 void CreateTriangle()
 {
-	unsigned int indices[] = {
-		0, 3, 1,
-		1, 3, 2,
-		2, 3, 0,
-		0, 1, 2
-	};
-
 	// Vertex data
 	GLfloat vertices[] = {
 		-1.0f, -1.0f, 0.0f,
-		0.0f, -1.0f, 1.0f,
 		1.0f, -1.0f, 0.0f,
 		0.0f, 1.0f, 0.0f
 	};
@@ -76,11 +52,6 @@ void CreateTriangle()
 	// Generate vertex array object (VAO)
 	glGenVertexArrays(1, &VAO);
 	glBindVertexArray(VAO);
-
-	// Generate element buffer object (IBO)
-	glGenBuffers(1, &IBO);
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IBO);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
 	// Generate vertex buffer object (VBO)
 	glGenBuffers(1, &VBO);
@@ -91,9 +62,8 @@ void CreateTriangle()
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
 	glEnableVertexAttribArray(0);
 
-	// Unbind VBO, IBO and VAO
+	// Unbind VBO and VAO
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 	glBindVertexArray(0);
 }
 
@@ -168,9 +138,8 @@ void CompileShaders()
 		return;
 	}
 
-	// ...
-	uniformModel = glGetUniformLocation(shader, "model");
-	uniformProjection = glGetUniformLocation(shader, "projection");
+	// Get uniform location for xMove variable in the vertex shader
+	uniformXMove = glGetUniformLocation(shader, "xMove");
 }
 
 
@@ -226,17 +195,12 @@ int main()
 		return 1;
 	}
 
-	// Enable Z-test
-	glEnable(GL_DEPTH_TEST);
-
 	// Setup Viewport size
 	glViewport(0, 0, bufferWidth, bufferHeight);
 
 	// Create triangle and compile shaders
 	CreateTriangle();
 	CompileShaders();
-
-	glm::mat4 projection = glm::perspective(45.0f, (GLfloat)bufferWidth / (GLfloat)bufferHeight, 0.1f, 1000.0f);
 
 	// Loop until window closed
 	while (!glfwWindowShouldClose(mainWindow))
@@ -258,54 +222,19 @@ int main()
 			direction = !direction;
 		}
 
-		curAngle += 0.05f;
-		if (curAngle >= 360)
-		{
-			curAngle = 0.0f;
-		}
-
-		if (curSize >= maxSize || curSize <= minSize)
-		{
-			sizeDirection = !sizeDirection;
-		}
-		if (sizeDirection)
-		{
-			curSize += 0.001f;
-		}
-		else
-		{
-			curSize -= 0.001f;
-		}
-
 		// Clear window
 		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		glClear(GL_COLOR_BUFFER_BIT);
 
 		// Use shader program
 		glUseProgram(shader);
 
-		// Diagonal + rotational translations
-		glm::mat4 model = glm::mat4(1.0f);
-		model = glm::translate(model, glm::vec3(triOffset, -triOffset, -2.5f));
-		model = glm::rotate(model, curAngle * toRadians, glm::vec3(0.0f, 1.0f, 0.0f));
-		model = glm::scale(model, glm::vec3(0.4f, 0.4f, 1.0f));
-		//model = glm::scale(model, glm::vec3(curSize, curSize, 1.0f));
-		//model = glm::translate(model, glm::vec3(triOffset, triOffset, 0.0f));
-
-
-
 		// Set uniform variable value for xMove in vertex shader
-		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
-		glUniformMatrix4fv(uniformProjection, 1, GL_FALSE, glm::value_ptr(projection));
+		glUniform1f(uniformXMove, triOffset);
 
 		// Bind VAO and draw triangle
 		glBindVertexArray(VAO);
-		//glDrawArrays(GL_TRIANGLES, 0, 3);
-		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IBO);
-
-		glDrawElements(GL_TRIANGLES, 12, GL_UNSIGNED_INT, 0);
-
-		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
 		glBindVertexArray(0);
 
 		// Unuse shader program
