79cc8bbcef1f68828fb9374302a80895e567d9bc
Denys S <counter297@gmail.com>
2024-05-15 09:05:06 +0200

    Cleanup old triangles

--- OpenGL_semestral_project/main.cpp
+++ OpenGL_semestral_project/main.cpp
@@ -61,33 +61,6 @@ static const char* vShader = "Shaders/shader.vert";
 // Fragment Shader
 static const char* fShader = "Shaders/shader.frag";
 
-// Function to create a triangle
-void CreateObjects()
-{
-	unsigned int indices[] = {
-		0, 3, 1,
-		1, 3, 2,
-		2, 3, 0,
-		0, 1, 2
-	};
-
-	// Vertex data
-	GLfloat vertices[] = {
-		//	x      y      z			u	  v
-		-1.0f, -1.0f, 0.0f,		0.0f, 0.0f,
-		0.0f, -1.0f, 1.0f,		0.5f, 0.0f,
-		1.0f, -1.0f, 0.0f,		1.0f, 0.0f,
-		0.0f, 1.0f, 0.0f,		0.5f, 1.0f
-	};
-
-	Mesh* obj1 = new Mesh();
-	obj1->CreateMesh(vertices, indices, 20, 12);
-	meshList.push_back(obj1);
-
-	Mesh* obj2 = new Mesh();
-	obj2->CreateMesh(vertices, indices, 20, 12);
-	meshList.push_back(obj2);
-}
 
 void CreateShaders()
 {
@@ -102,7 +75,6 @@ int main()
 	mainWindow = Window(800, 600);
 	mainWindow.Initialise();
 
-	CreateObjects();
 	CreateShaders();
 
 	camera = Camera(glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f), -90.0f, 0.0f, 5.0f, 0.5f);
@@ -170,39 +142,7 @@ int main()
 		 
 		 camera.keyControl(mainWindow.getKeys(), deltaTime);
 		 camera.mouseControl(mainWindow.getXChange(), mainWindow.getYChange());
-		 
-		 // Update triangle offset for animation
-		 if (direction)
-		 {
-		 	triOffset += triIncrement;
-		 }
-		 else {
-		 	triOffset -= triIncrement;
-		 }
-		 
-		 if (abs(triOffset) >= triMaxOffset)
-		 {
-		 	direction = !direction;
-		 }
-		 
-		 curAngle += 0.05f;
-		 if (curAngle >= 360)
-		 {
-		 	curAngle = 0.0f;
-		 }
-		 
-		 if (curSize >= maxSize || curSize <= minSize)
-		 {
-		 	sizeDirection = !sizeDirection;
-		 }
-		 if (sizeDirection)
-		 {
-		 	curSize += 0.001f;
-		 }
-		 else
-		 {
-		 	curSize -= 0.001f;
-		 }
+
 		 
 		 // Clear window
 		 glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
@@ -229,34 +169,15 @@ int main()
 		 
 		 // Diagonal + rotational translations
 		 glm::mat4 model = glm::mat4(1.0f);
-		 //model = glm::rotate(model, curAngle * toRadians, glm::vec3(0.0f, 1.0f, 0.0f));
-		 model = glm::translate(model, glm::vec3(0.0f, -0.0f, -2.5f));
-		 model = glm::scale(model, glm::vec3(0.4f, 0.4f, 1.0f));
-		 model = glm::translate(model, glm::vec3(-triOffset, -triOffset, 0.0f));
-		 model = glm::rotate(model, -curAngle * toRadians, glm::vec3(0.0f, 1.0f, 0.0f));
 		 
 		 
 		 // Set uniform variable value for xMove in vertex shader
 		 glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
 		 glUniformMatrix4fv(uniformProjection, 1, GL_FALSE, glm::value_ptr(projection));
 		 glUniformMatrix4fv(uniformView, 1, GL_FALSE, glm::value_ptr(camera.calculateViewMatrix()));
-		 brickTexture.UseTexture();
-		 meshList[0]->RenderMesh();
-		 
-		 
+
 		 model = glm::mat4(1.0f);
-		 //model = glm::rotate(model, -curAngle * toRadians, glm::vec3(0.0f, 1.0f, 0.0f));
-		 model = glm::translate(model, glm::vec3(0.0f, 1.0f, -10.0f));
-		 model = glm::scale(model, glm::vec3(0.4f, 0.4f, 1.0f));
-		 model = glm::translate(model, glm::vec3(triOffset, triOffset, 0.0f));
-		 model = glm::rotate(model, -curAngle * toRadians, glm::vec3(0.0f, 1.0f, 0.0f));
-		 glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
-		 //glUniformMatrix4fv(uniformProjection, 1, GL_FALSE, glm::value_ptr(camera.calculateViewMatrix()));
-		 dirtTexture.UseTexture();
-		 meshList[1]->RenderMesh();
-		 
-		 model = glm::mat4(1.0f);
-		 model = glm::translate(model, glm::vec3(-7.0f, 10.0f, 0.0f));
+		 model = glm::translate(model, glm::vec3(-5.0f, 2.0f, 0.0f));
 		 model = glm::scale(model, glm::vec3(0.006f, 0.006f, 0.006f));
 		 glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
 		 shinyMaterial.UseMaterial(uniformSpecularIntensity, uniformShininess);
